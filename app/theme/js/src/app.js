07e74679722898159eb4e414b8a6983cf3792aef
Thomas Kotowicz <kotowicztom@gmail.com>
2017-08-19 19:02:03 +0100

    gulp files

--- app/theme/js/src/app.js
+++ app/theme/js/src/app.js
@@ -22,6 +22,24 @@ $(document).ready(() => {
 	window.TK = TK;
 
 	TK.init();
+
+	new Vue({
+		el: '#app',
+		data: {
+			title: "Hello world 2",
+			link: "http://google.com"
+		},
+		methods: {
+			changeTitle: function(e) {
+				this.title = e.target.value;
+			},
+			sayHello: function() {
+				return this.title;
+			}
+		},
+	});
+
+
 });
 
 export default TK;
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<head>
 		<meta charset="utf-8">
 	        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>Website</title>
+		<title>Vue</title>
 
 		<meta property="og:title" content="">
 	    <meta property="og:description" content="">
@@ -18,7 +18,11 @@
 		<link rel="stylesheet" href="/app/theme/css/<?= Utils::getAssetRevision( "master.min.css", "styles" ) ?>" type="text/css" />
 	</head>
 	<body id="body">
-		
+			
+	<div id="app">
+		<input type="text" v-on:input="changeTitle">
+		<p>  {{ sayHello() }} - <a v-bind:href="link">Google</a></p>
+	</div>
 
 		<script src="/app/theme/js/<?= Utils::getAssetRevision( "plugins.min.js", "plugin-scripts" ) ?>"></script>
 		<script src="/app/theme/js/<?= Utils::getAssetRevision( "app.min.js", "scripts" ) ?>"></script>
